d936fc05a904e065af2d945ffa2b9023e31b8011
paulvvo <paulvo@live.ca>
2018-10-17 22:53:04 -0400

    tried to implement masonry library, did not work. further research needed

--- src/Components/IdeaPreviewBox/IdeaPreviewBox.js
+++ src/Components/IdeaPreviewBox/IdeaPreviewBox.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class IdeaPreviewBox extends Component{
 	render(){
 		return(
-			<div>
+			<div className="grid-item">
 				<img
 				className="IdeaPreviewImage"
 				src= {this.props.url}
