96c6d17f16576cd97c1502f80ead94d055374b75
paulvvo <paulvo@live.ca>
2018-10-18 23:27:12 -0400

    began creation of login form

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Login extends Component{
+	render(){
+		return(
+			<div>
+				<input type="text" placeholder="Enter Your Email"/>
+			</div>
+		)
+	}
+}
+
+export default Login;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Components
-
+import Login from "../Components/Login/Login";
 import IdeaPreviewBoxContainer from "../Components/IdeaPreviewBoxContainer/IdeaPreviewBoxContainer";
 
 class App extends Component {
   render() {
     return (
     	<div>
+			<Login/>
 				<IdeaPreviewBoxContainer/>
 			</div>
     );
