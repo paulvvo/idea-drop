d9dde0c8b0c7e1135587b00d8b7070bc575c9428
paulvvo <paulvo@live.ca>
2018-10-30 23:24:15 -0400

    removed react-masonry-component and removed gallery component, no longer using masonry effect for this project

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,30 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Components
-import Gallery from "../Components/Gallery/Gallery";
-// import Login from "../Components/Login/Login";
-// import Register from "../Components/Register/Register";
+
+import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 import NavBar from "../Components/NavBar/NavBar";
-// import IdeaPreviewBoxContainer from "../Components/IdeaPreviewBoxContainer/IdeaPreviewBoxContainer";
+import IdeaPreviewBoxContainer from "../Components/IdeaPreviewBoxContainer/IdeaPreviewBoxContainer";
 
 class App extends Component {
 	constructor(){
 		super();
 		this.state ={
-			elements:[
-				{src:"https://images.unsplash.com/photo-1530061738406-233b0e977ea7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b652fbe3813716c0f321ddd75fa85a6&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1519669556878-63bdad8a1a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1657bf457f194ab14b488008077f52&auto=format&fit=crop&w=751&q=80"},
-				{src:"https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a99fc6885bdf5c5e71eaaabffb5561b&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1531956759688-e71cc2586e34?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be729fa3e0acdadd5763113a9da5c966&auto=format&fit=crop&w=334&q=80"},
-				{src:"https://images.unsplash.com/photo-1530061738406-233b0e977ea7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b652fbe3813716c0f321ddd75fa85a6&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1519669556878-63bdad8a1a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1657bf457f194ab14b488008077f52&auto=format&fit=crop&w=751&q=80"},
-				{src:"https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a99fc6885bdf5c5e71eaaabffb5561b&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1531956759688-e71cc2586e34?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be729fa3e0acdadd5763113a9da5c966&auto=format&fit=crop&w=334&q=80"},
-				{src:"https://images.unsplash.com/photo-1530061738406-233b0e977ea7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b652fbe3813716c0f321ddd75fa85a6&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1519669556878-63bdad8a1a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2d1657bf457f194ab14b488008077f52&auto=format&fit=crop&w=751&q=80"},
-				{src:"https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a99fc6885bdf5c5e71eaaabffb5561b&auto=format&fit=crop&w=400&q=60"},
-				{src:"https://images.unsplash.com/photo-1531956759688-e71cc2586e34?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be729fa3e0acdadd5763113a9da5c966&auto=format&fit=crop&w=334&q=80"}
-			]
 		}
 	}
 	componentDidMount(){
@@ -34,8 +20,10 @@ class App extends Component {
   render() {
     return (
     	<div>
-				<Gallery elements={this.state.elements}/>
 				<NavBar/>
+				<IdeaPreviewBoxContainer/>
+				<Register/>
+				<Login/>
 
 			</div>
     );
@@ -43,7 +31,3 @@ class App extends Component {
 }
 
 export default App;
-
-		// <IdeaPreviewBoxContainer/>
-// <Register/>
-// <Login/>
